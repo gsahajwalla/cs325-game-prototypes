ccbe7a5085d2a73be2444a98d49b80b21c21768c
gsahajwalla <gsahajwalla@gmail.com>
2018-02-06 02:19:30 -0500

    added ground

--- digital-game-1/js/main.js
+++ digital-game-1/js/main.js
@@ -3,22 +3,38 @@
 window.onload = function() {
 
 
-    var game = new Phaser.Game( 1000, 600, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update } );
+    var game = new Phaser.Game( 700, 600, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update } );
 
     function preload() {
         // Load an image and call it 'logo'.
-        game.load.image('background','assets/background.png');
+        game.load.image('background', 'assets/background.png');
+        game.load.image('ground', 'assets/ground.png');
 
     }
 
-    var cave, background;
+    var background, ground, platform;
 
     function create() {
 
-      
+      //enable physics for the game
+      game.physics.startSystem(Phaser.Physics.ARCADE);
+
+      //the background of the game
       background = game.add.sprite(0,0,'background');
       background.height = game.height;
       background.width = game.width;
+
+      //the ground in the game
+      ground = game.add.sprite(0,560,'ground');
+      ground.width = game.width;
+      //keeps the player from falling away when the player jumps on it
+      ground.body.immovable = true;
+
+      //this is grouping helps group sprites 
+      platform = game.add.group();
+
+
+
     }
 
     function update() {
