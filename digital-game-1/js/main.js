4fa97042f0765e678fd60a1a23f7fb6e7f3658dd
gsahajwalla <gsahajwalla@gmail.com>
2018-02-06 01:58:48 -0500

    modified main.js by adding background, also added new background image and a source.txt file

--- digital-game-1/js/main.js
+++ digital-game-1/js/main.js
@@ -3,18 +3,22 @@
 window.onload = function() {
 
 
-    var game = new Phaser.Game( 1000, 700, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update } );
+    var game = new Phaser.Game( 1000, 600, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update } );
 
     function preload() {
         // Load an image and call it 'logo'.
-        game.load.image( 'cave', 'assets/cave.png' );
+        game.load.image('background','assets/background.png');
+
     }
 
-    var cave;
+    var cave, background;
 
     function create() {
 
-      cave = game.add.sprite( 0, 0, 'cave' );
+      
+      background = game.add.sprite(0,0,'background');
+      background.height = game.height;
+      background.width = game.width;
     }
 
     function update() {
