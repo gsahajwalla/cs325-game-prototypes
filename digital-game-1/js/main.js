c03be509cbe6073e814b6750e25ff15296cc81be
gsahajwalla <gsahajwalla@gmail.com>
2018-02-07 19:42:19 -0500

    enable physics,added player and controls to the player

--- digital-game-1/js/main.js
+++ digital-game-1/js/main.js
@@ -1,5 +1,5 @@
 
-
+var background, ground, platform, player, cursors;
 window.onload = function() {
 
 
@@ -9,11 +9,14 @@ window.onload = function() {
         // Load an image and call it 'logo'.
         game.load.image('background', 'assets/background.png');
         game.load.image('ground', 'assets/ground.png');
-        game.load.image('platform', 'assets/platform.png');
+        //game.load.image('platform', 'assets/platform.png');
+        //this player sprite is taken from phaser tutorial
+        game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+
 
     }
 
-    var background, ground, platform;
+    
 
     function create() {
 
@@ -26,13 +29,52 @@ window.onload = function() {
       background.width = game.width;
 
       //ground of the game
-      //ground.enableBody = true;
       ground = game.add.sprite(0,game.height - 60,'ground');
       ground.scale.setTo(2,2);
+      game.physics.arcade.enable(ground);
+      ground.body.allowGravity = false;
       ground.body.immovable = true;
+
+      //The code for player is made follwoing the phaser tutorial
+      //this is the player 
+      player = game.add.sprite(32, game.height - 110, 'dude');
+      //enable physeics for the player 
+      game.physics.arcade.enable(player);
+
+      player.body.collideWorldBounds = true;
+
+      //animation 
+      player.animations.add('left', [0, 1, 2, 3], 10, true);
+      player.animations.add('right', [5, 6, 7, 8], 10, true);
+      player.body.gravity.y = 500;
+
+
     }
 
     function update() {
+      //checks if the player collided with the ground. Taken from tutorial
+      var hitPlatform = game.physics.arcade.collide(player, ground);
 
+      player.body.velocity.x = 0;
+
+      cursors = game.input.keyboard.createCursorKeys();
+
+      //add controls for the player to move (written following phaser tutorial)
+      if(cursors.left.isDown) {
+        player.body.velocity.x = -50;
+        player.animations.play('left');
+      }
+      else if(cursors.right.isDown) {
+        player.body.velocity.x = 50;
+        player.animations.play('right');
+      }
+      else {
+        player.animations.stop();
+        player.frame = 4;
+      }
+
+      if (cursors.up.isDown && player.body.touching.down) {
+        player.body.velocity.y = -200;
+      }
     }
 };
