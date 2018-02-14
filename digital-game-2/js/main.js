6d7d0317fcd7bd5a086874a461dd503e7e2ac459
gsahajwalla <gsahajwalla@gmail.com>
2018-02-14 01:51:05 -0500

    added chicks and sleep and also made all the interaction buttons functional

--- digital-game-2/js/main.js
+++ digital-game-2/js/main.js
@@ -6,8 +6,11 @@ var GameState1 = {
         this.load.spritesheet('chicken','assets/chicken.png');
         this.load.image('background','assets/background.png');
         this.load.image('seed','assets/seed.png');
+        this.load.image('chick','assets/chick.png');
         this.load.image('pet','assets/pet.png');
         this.load.image('seeds','assets/seeds.png');
+        this.load.image('chicks','assets/chicks.png');
+        this.load.image('sleep','assets/sleep.jpg');
     },
     create: function() {
 
@@ -36,21 +39,61 @@ var GameState1 = {
         //allows input by keyboard
         this.cursors = this.game.input.keyboard.createCursorKeys();
 
-        //the food for the chicken
-        this.seed;
-        let a = this;//so that this represnts gamestate1 and not world
-        this.game.input.onTap.add(function() {
-            a.seed = a.game.add.sprite( Math.random() * 1030, (Math.random() * (355)) + 250,'seed');
-            a.seed.lifespan = 10000;
-            console.log(a);
-        });
 
 
         //this is the pet button 
         this.pet =  this.game.add.sprite(300,560,'pet');
+        //enables input for pet 
+        this.pet.inputEnabled = true;
+        this.pet.input.pixelPerfect = true;
+        //pets chicken to increase love on being clicked
+        this.pet.events.onInputDown.add(this.petChicken,this);
+        this.pet.amount = 10;
+        this.pet.health = 0;
+        this.pet.love = 10;
+
 
         //this is the seeds button
         this.seeds = this.game.add.sprite(400,560,'seeds');
+        //enables input for seeds
+        this.seeds.inputEnabled = true;
+        this.seeds.input.pixelPerfect = true;
+        //give seed on being clicked
+        this.seeds.events.onInputDown.add(this.giveSeed,this);
+        this.seeds.amount = 10;
+        this.seeds.health = 10;
+        this.seeds.love = 0;
+
+
+        //this button is to feed the chicken chicks (adds alot of health but also will drastically decrease the love)
+        this.chicks = this.game.add.sprite(500,560,'chicks');
+        //enables input for the chicks button 
+        this.chicks.inputEnabled = true;
+        this.chicks.input.pixelPerfect = true;
+        this.chicks.events.onInputDown.add(this.giveChick,this);
+        this.chicks.amount = 5;
+        this.chicks.health = 70;
+        this.chicks.love = -35;
+
+        //this is the sleep button, increases health drastically 
+        this.sleep = this.game.add.sprite(600,560,'sleep');
+        this.sleep.inputEnabled = true;
+        this.sleep.input.pixelPerfect = true;
+        this.sleep.events.onInputDown.add(this.chickenSleep,this);
+        this.sleep.amount = 5;
+        this.sleep.health = 15;
+        this.sleep.love = 15;
+
+        //this array is an array of all the buttons
+        this.interact = [this.seeds,this.pet,this.chicks,this.sleep];
+        //this is variable stores the currently active button 
+        this.currentInteraction = null;
+
+        //these two variables give details if the chicken is sleeping and eating 
+        this.sleeping = false;
+        this.eating = false;
+
+
     },
     update: function() {
         
@@ -76,6 +119,32 @@ var GameState1 = {
         }
 
         
+    },
+    giveSeed: function(sprite) {
+        if(sprite.amount >= 1) {
+            this.seed = this.game.add.sprite( Math.random() * 1030, (Math.random() * (310)) + 250,'seed');
+            this.seed.lifespan = 10000;
+            sprite.amount -= 1;
+        }
+    },
+    petChicken: function(sprite) {
+        if(sprite.amount >= 1) {
+            console.log("pet chicken");
+            sprite.amount -= 1;
+        }
+    },
+    giveChick: function(sprite) {
+        if(sprite.amount >= 1) {
+            this.chick = this.game.add.sprite( Math.random() * 1030, (Math.random() * (310)) + 250,'chick');
+            this.chick.lifespan = 10000;
+            sprite.amount -= 1;
+        }
+    },
+    chickenSleep: function(sprite) {
+        if(sprite.amount >= 1) {
+            console.log("sleep chicken");
+            sprite.amount -= 1;
+        }
     }
 };
 
