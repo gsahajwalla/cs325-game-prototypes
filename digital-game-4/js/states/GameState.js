359d2603214680678db631b857b17442b5fe72fd
gsahajwalla <gsahajwalla@gmail.com>
2018-03-01 05:50:32 -0500

    DA4 finshed game mechanics

--- digital-game-4/js/states/GameState.js
+++ digital-game-4/js/states/GameState.js
@@ -35,36 +35,47 @@ var GameState = {
 		this.h1.animations.play('walk',9,true);
 		this.game.physics.arcade.enable(this.h1);
 		this.h1.body.collideWorldBounds = true;
+		this.h1.p = this.positions[0];
+		this.setVelocities(this.h1);
 		
 		this.h2 = this.game.add.sprite(05,320,'horse');
 		this.h2.animations.add('walk',[3,4,5]);
 		this.h2.animations.play('walk',9,true);
 		this.game.physics.arcade.enable(this.h2);
 		this.h2.body.collideWorldBounds = true;
+		this.h2.p = this.positions[1];
+		this.setVelocities(this.h2);
+
 
 		this.h3 = this.game.add.sprite(05,420,'horse');
 		this.h3.animations.add('walk',[3,4,5]);
 		this.h3.animations.play('walk',9,true);
 		this.game.physics.arcade.enable(this.h3);
 		this.h3.body.collideWorldBounds = true;
+		this.h3.p = this.positions[2];
+		this.setVelocities(this.h3);
+
 
 		this.h4 = this.game.add.sprite(05,520,'horse');
 		this.h4.animations.add('walk',[3,4,5]);
 		this.h4.animations.play('walk',9,true);
 		this.game.physics.arcade.enable(this.h4);
 		this.h4.body.collideWorldBounds = true;
+		this.h4.p = this.positions[3];
+		this.setVelocities(this.h4);
+
 
 		// the velocities
 		//this.velocity = [this.h1.body.velocity.x,this.h2.body.velocity.x,this.h3.body.velocity.x,this.h4.body.velocity.x];
 		this.velocity = [0,0,0,0]
-		console.log(this.velocity)
-		this.setVelocities();
+		//console.log(this.velocity)
+		//this.setVelocities();
 		console.log(this.velocity)
 	},
 
 	update: function() {
 		if(this.game.physics.arcade.collide(this.h1, this.finish) || this.game.physics.arcade.collide(this.h2, this.finish) || this.game.physics.arcade.collide(this.h3, this.finish) || this.game.physics.arcade.collide(this.h4, this.finish)) {
-
+			this.declareWinner();
 		}
 	},
 
@@ -82,18 +93,26 @@ var GameState = {
 			//console.log(a);
 			this.positions.push(this.standings.indexOf(a) + 1);
 			this.standings[this.standings.indexOf(a)] = -1;
-			//console.log(this.standings);
-			//console.log(this.positions);
+			
 
 		}
+		
+			console.log(this.positions);
 	},
-	setVelocities: function() {
-		for(let i = 0; i < 4; i++) {
-			this.velocity[this.positions[i] - 1] = 100 - (i * 10);
-		}
-		this.h1.body.velocity.x = this.velocity[0];
-		this.h2.body.velocity.x = this.velocity[1];
-		this.h3.body.velocity.x = this.velocity[2];
-		this.h4.body.velocity.x = this.velocity[3];
+	setVelocities: function(sprite) {
+		// for(let i = 0; i < 4; i++) {
+		// 	this.velocity[this.positions[i] - 1] = 100 - (i * 10);
+		// }
+		// this.h1.body.velocity.x = this.velocity[this.positions[0] - 1];
+		// this.h2.body.velocity.x = this.velocity[this.positions[1] - 1];
+		// this.h3.body.velocity.x = this.velocity[this.positions[2] - 1];
+		// this.h4.body.velocity.x = this.velocity[this.positions[3] - 1];
+		sprite.body.velocity.x = 110 - (sprite.p * 10);
+	},
+	declareWinner: function() {
+		var text;
+		var style = { font: "bold 32px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
+		this.text = game.add.text(0, 0, "Player's Horse #" + this.playerHorse + " came " + this.positions[this.playerHorse - 1] , style);
+		//this.game.state.restart();
 	}
 };
